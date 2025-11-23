2668897ab12fe7e3791eb23547ebd135ee293e1b
abibernardo <153117023+abibernardo@users.noreply.github.com>
2025-11-23 20:21:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+# Configurações gerais do app
+st.set_page_config(
+    page_title="Avaliação do Impacto da Priori na inferência para árvores de contexto Bayesianas",
+    page_icon="📊",
+    layout="wide"
+)
