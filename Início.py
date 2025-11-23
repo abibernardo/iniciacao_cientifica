d13595e6b8ad55e43ae40722351aaed4e43a8a87
abibernardo <153117023+abibernardo@users.noreply.github.com>
2025-11-23 20:40:23 -0300

    Update and rename app.py to Início.py

---
+++ Início.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Configurações gerais do app
+st.set_page_config(
+    page_title="Iniciação Científica",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("Avaliação do Impacto da Priori na inferência para árvores de contexto Bayesianas")
--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Configurações gerais do app
-st.set_page_config(
-    page_title="Avaliação do Impacto da Priori na inferência para árvores de contexto Bayesianas",
-    page_icon="📊",
-    layout="wide"
-)
