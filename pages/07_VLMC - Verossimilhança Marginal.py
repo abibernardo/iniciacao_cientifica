3328deb4dbb8cee1947c538567842aeba8d0f9a0
abibernardo <153117023+abibernardo@users.noreply.github.com>
2026-05-03 20:07:02 -0300

    Update 07_VLMC - Verossimilhança Marginal.py

--- pages/07_VLMC - Verossimilhança Marginal.py
+++ pages/07_VLMC - Verossimilhança Marginal.py
@@ -1,8 +1,90 @@
 import numpy as np
 from scipy.special import gammaln
 
-# =========================
-# SUA FUNÇÃO (mantida)
+arvore = {
+
+    # Hoje é A
+    "A": [0.5, 0.3, 0.2],
+
+    # Hoje é B
+    "B": {
+
+        # Ontem foi C
+        "C": [0.2, 0.5, 0.3],
+        # Ontem foi B
+        "B": [0.3, 0.4, 0.3],
+        # Ontem foi A
+        "A": {
+            # Anteontem foi A:
+            "A": [0.7, 0.2, 0.1],
+            # Anteontem foi B:
+            "B": [0.4, 0.4, 0.2],
+            # Anteontem foi C:
+            "C": [0.5, 0.3, 0.2]
+        }
+    },
+
+    # Hoje é C
+    "C": {
+        # Ontem foi A
+        "A": [0.3, 0.4, 0.3],
+        # Ontem foi B
+        "B": [0.2, 0.5, 0.3],
+        # Ontem foi C
+        "C": [0.4, 0.3, 0.3]
+    }
+}
+
+def achar_contexto(arvore, historico):
+    contexto = arvore
+
+    # percorre do mais recente para o mais antigo.
+    # Se historico = [anteontem, ontem, hoje], então partimos de 'hoje' até 'anteontem'
+    for s in list(reversed(historico)):
+
+        # se já estamos numa folha → contexto encontrado, retona folha com probabilidades
+        if isinstance(contexto, list):
+            return contexto
+
+        # se existe ramo correspondente, desce a árvore (estado anterior) e retorna ao loop
+        if s in contexto:
+            contexto = contexto[s]
+        # se não existe ramo correspondente, sai do loop
+        else:
+            break  # não há mais aprofundamento possível
+
+    # se terminamos numa lista, retornamos a folha com as probabilidades
+    if isinstance(contexto, list):
+        return contexto
+    # Caso contrário, o historico não existe na árvore
+    raise ValueError("Nenhum contexto encontrado.")
+
+def simular_vlmc(arvore, estados, T, pi, ordem_max):
+    # Passado inicial gerado da distribuição inicial
+    X = [np.random.choice(estados, p=pi) for _ in range(ordem_max)]
+
+    # Evolução da cadeia
+    for t in range(ordem_max, T):
+
+        #Pega os últimos ordem_max estados do histórico, sem inverter a ordem (do mais antigo até o mais novo)
+        historico = X[-ordem_max:]
+
+        # Encontramos as probs de transição com a função auxiliar
+        probs = achar_contexto(arvore, historico)
+
+        # Sorteamos o próximo estado segundo essa distribuição
+        proximo = np.random.choice(estados, p=probs)
+
+        # Acrescentamos à sequência
+        X.append(proximo)
+
+    return X
+
+
+estados = ["A", "B", "C"]
+pi = np.array([0.5, 0.3, 0.2])
+X = simular_vlmc(arvore, estados, T=2000, pi=pi, ordem_max=3)
+
 # =========================
 def contagem_contextos(historico, ordem_max):
     contagens_transicao = {}
@@ -35,7 +117,7 @@ def extrair_contextos(arvore, prefixo=()):
             contextos.extend(extrair_contextos(valor, novo_prefixo))
         else:
             # folha → contexto válido
-            contextos.append(tuple(reversed(novo_prefixo)))
+            contextos.append(tuple(novo_prefixo)) # do mais recente ao mais antigo, igual a árvore
 
     return contextos
 
@@ -77,26 +159,7 @@ def log_Q_alpha(counts, alpha):
 # =========================
 
 # Sua árvore
-arvore = {
-    "A": [0.5, 0.3, 0.2],
-    "B": {
-        "C": [0.2, 0.5, 0.3],
-        "B": [0.3, 0.4, 0.3],
-        "A": {
-            "A": [0.7, 0.2, 0.1],
-            "B": [0.4, 0.4, 0.2],
-            "C": [0.5, 0.3, 0.2]
-        }
-    },
-    "C": {
-        "A": [0.3, 0.4, 0.3],
-        "B": [0.2, 0.5, 0.3],
-        "C": [0.4, 0.3, 0.3]
-    }
-}
 
-# Histórico de estados (exemplo)
-X = ["A","B","A","A","C","B","A","B","C","A","A"]
 
 # Alfabeto
 alfabeto = ["A", "B", "C"]
